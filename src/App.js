5fb8a6354fc5f14f3b423394efd787f327da4ea9
etstringy <eddyteddyt@gmail.com>
2019-10-29 10:11:55 +0000

    Okay, fuck it. Let's start from scratch.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/strsft.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stringy Software</h1>
       </header>
     </div>
   );
