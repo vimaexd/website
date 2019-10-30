871c4f7dc12de6a69cb96833636dcaaf84719d01
etstringy <eddyteddyt@gmail.com>
2019-10-30 09:58:11 +0000

    stuff 2.0 - fontawesome

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -3,17 +3,42 @@ import { Link } from 'react-router-dom';
 import './MainMenu.css';
 import './App.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDiscord, faTwitter, faGithub, faSpotify, faTwitch, faYoutube} from '@fortawesome/free-brands-svg-icons';
+
+
+library.add(
+  faDiscord,
+  faTwitter,
+  faGithub,
+  faSpotify,
+  faTwitch,
+  faYoutube
+);
+
 class MainMenu extends React.Component {
     render() {
         return (
             <div className="MainMenu">
-                <Link to="/projects">
-                    <button className="MainMenu-Grad1">Projects</button>
-                </Link>
-                <Link to="/music">
-                    <button className="MainMenu-Grad2">Music</button>
-                </Link>
+                {/* <div className="MainMenu-Buttons">
+                    <Link to="/projects">
+                        <button className="MainMenu-Grad1">Projects</button>
+                    </Link>
+                    <Link to="/music">
+                        <button className="MainMenu-Grad2">Music</button>
+                    </Link>
+                </div> */}
+                <div className="MainMenu-Social">
+                    <a href="https://discord.gg/E8Vf4ZR"><FontAwesomeIcon className="MainMenu-SocialButton S-discord" icon={faDiscord} size="2x"/></a> 
+                    <a href="https://github.com/etstringy"><FontAwesomeIcon className="MainMenu-SocialButton S-github" icon={faGithub} size="2x"/></a>
+                    <a href="https://twitter.com/stringyofficial"><FontAwesomeIcon className="MainMenu-SocialButton S-twitter" icon={faTwitter} size="2x"/></a>
+                    <a href="https://open.spotify.com/artist/6Ruuwji9E40ngpwS0wwxTN"><FontAwesomeIcon className="MainMenu-SocialButton S-spotify" icon={faSpotify} size="2x"/></a>
+                    <a href="https://twitch.tv/etstringy"><FontAwesomeIcon className="MainMenu-SocialButton S-twitch" icon={faTwitch} size="2x"/></a>
+                    <a href="https://youtube.com/Stringy"><FontAwesomeIcon className="MainMenu-SocialButton S-youtube" icon={faYoutube} size="2x"/></a>
+                </div>
             </div>
+
         )
     }
 }
