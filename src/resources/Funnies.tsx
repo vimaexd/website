68cf2fd92345303c83f7889a4a059cf95ee82f27
etstringy <eddyteddyt@gmail.com>
2021-02-09 13:10:28 +0000

    re add twinimi's line
    awa
    
    Co-Authored-By: Twinimi13 <48106763+twinimi13@users.noreply.github.com>

--- src/resources/Funnies.tsx
+++ src/resources/Funnies.tsx
@@ -18,10 +18,10 @@ export default [
     "bodge",
     "delete this",
     "get in the jar",
-    "EvaX was here",
     "600 When 600 When 600 When 600 When",
     "Cannot BRUH /",
     "Don't let the Dalux bite.",
     "bodge was here - meep",
-    "where are the bobux"
+    "where are the bobux",
+    "EvaX was here"
 ]
