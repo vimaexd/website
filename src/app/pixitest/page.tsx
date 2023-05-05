56a1183b11d07376751523582cff6cd9ff79b853
Mae <stringy@mae.wtf>
2023-05-05 21:07:18 +0100

    remove pixi test

--- src/app/pixitest/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BlurFilter } from 'pixi.js';
-import { Stage, Container, Sprite, Text } from '@pixi/react';
-import { useMemo } from 'react';
-
-export default function page() {
-  return (
-    <div><h1>im ganna kill someone</h1></div>
-  );
-}
