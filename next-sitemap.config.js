f829750256ada27beb41ae76dd089f8de502918c
etstringy <stringy@mae.wtf>
2023-10-13 12:15:35 +0100

    📘 sitemap generate discog and blog

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,24 @@
+const fs = require('fs/promises')
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://mae.wtf",
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  async additionalPaths() {
+    let paths = []
+    
+    // discog
+    const discography = await import('./src/data/discography.json');
+    discography.releases.forEach((r) => {
+      paths.push("/music/release/" + r.slug)
+    })
+
+    // blog
+    const blogdir = await fs.readdir('blog');
+    blogdir.forEach((b) => {
+      paths.push("/blog/" + b)
+    })
+
+    return paths;
+  }
 }
