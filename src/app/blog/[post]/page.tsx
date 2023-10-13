400dbbda94db11c30050e59077c39f6b020cb3ae
etstringy <stringy@mae.wtf>
2023-10-13 12:10:47 +0100

    🗣️ metadata for blog posts

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -6,6 +6,33 @@ import { notFound } from 'next/navigation';
 import {remark} from 'remark'
 import remarkHtml from 'remark-html'
 import BlogPost from './BlogPost';
+import { Metadata } from 'next';
+
+export async function generateMetadata(
+  props: any,
+  parent: any
+): Promise<Metadata> {
+  const postPath = path.join(process.cwd(), 'blog', props.params.post)
+  try {
+    await fs.access(postPath, fs.constants.R_OK)
+  } catch(err) {
+    return {};
+  }
+
+  const meta = JSON.parse(await fs.readFile(path.join(postPath, 'post.json'), {encoding: "utf-8"}));
+
+  return {
+    title: meta.title,
+    description: "a blog post by mae on mae.wtf",
+    openGraph: {
+      authors: ['vimae'],
+      url: `https://mae.wtf/blog/${meta.id}`,
+      publishedTime: meta.date,
+      tags: ['post'].concat(meta.tags),
+      images: [`/assets/blog_img/${meta.id}/${meta.header_img}`]
+    },
+  }
+}
 
 export default async function page(props: any) {
   const postPath = path.join(process.cwd(), 'blog', props.params.post)
