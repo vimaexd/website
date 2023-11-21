f1bf964cfa51204e6e7689e82d6696de0f958bf8
etstringy <stringy@mae.wtf>
2023-11-21 10:48:22 +0000

    😀 fix the tooltips FINALLY

--- src/components/frontpageBlocks/sideblocks/SocialSpeeddialBlock.tsx
+++ src/components/frontpageBlocks/sideblocks/SocialSpeeddialBlock.tsx
@@ -5,7 +5,7 @@ import { Tooltip } from 'react-tooltip';
 export default function SocialSpeeddial({href, tooltipContent, tooltipId, icon}: {href: string, tooltipContent: string, tooltipId: string, icon: string}) {
   return (
     <Link href={href} data-tooltip-content={tooltipContent} data-tooltip-id={tooltipId} className='text-white' aria-label={tooltipContent}>
-      <Tooltip id={tooltipId} className='text-sm' />
+      <Tooltip id={tooltipId} style={{fontSize: "0.875rem", lineHeight: "1.25rem"}} />
       <i className={`bx ${icon} hover:scale-110 hover:text-str-yarn transition-all duration-75 active:scale-90`}></i>
     </Link>
   )
