ef8544d861963d79ea84ab326b802299c6d52d29
etstringy <stringy@mae.wtf>
2023-11-21 11:46:41 +0000

    🤍 add zaire badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,6 +191,7 @@ export default function Home() {
         <div className="flex justify-center flex-wrap gap-2">
           <WebBadge href="https://mae.wtf" src="/assets/buttons/mae.png" alt="Mae web button"></WebBadge>
           <WebBadge href="https://invoxiplaygames.uk" src="/assets/buttons/ipg.png" alt="InvoxiPlayGames web button"></WebBadge>
+          <WebBadge href="http://supitszaire.com" src="/assets/buttons/zaire.gif" alt="zaire web button"></WebBadge>
           <WebBadge href="javascript:void(0)" src="/assets/buttons/trans.png" alt="Transgender web button"></WebBadge>
           <WebBadge href="javascript:void(0)" src="/assets/buttons/bi.png" alt="Bisexual web button"></WebBadge>
         </div>
