9ace3aa6c247bf67d96b44cd8314b800eb0f07d6
Mae <stringy@mae.wtf>
2024-02-20 00:02:09 +0000

    use html for rss feed

--- src/misc/genRssFeed.ts
+++ src/misc/genRssFeed.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import fs from 'fs/promises';
 import dayjs from 'dayjs';
 import RSS from 'rss';
+import {remark} from 'remark'
+import remarkHtml from 'remark-html'
 
 export async function generateRSSFeed() {
   // generate rss feed from blog dir using rss package
@@ -33,10 +35,15 @@ export async function generateRSSFeed() {
 
   for await(let p of posts) {
     // fetch post
-    const content = await fs.readFile(path.join(postPath, p.id, p.file), {encoding: "utf-8"});
+    const rawContent = await fs.readFile(path.join(postPath, p.id, p.file), {encoding: "utf-8"});
+    
+    const content = await remark()
+    .use(remarkHtml)
+    .process(rawContent)
+
     feed.item({
       title: p.title,
-      description: content,
+      description: content.toString(),
       url: `https://mae.wtf/blog/${p.id}`,
       date: p.date,
       author: 'vimae',
