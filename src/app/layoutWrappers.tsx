90ac2f6f6ef2f66223c614f86a80338dbdf1d97e
mae <mae@mae.wtf>
2024-04-23 20:49:26 +0100

    fix plausible custom domain and bump ver

--- src/app/layoutWrappers.tsx
+++ src/app/layoutWrappers.tsx
@@ -7,7 +7,7 @@ import PlausibleProvider from 'next-plausible'
 export default function LayoutWrappers({children}: {children: ComponentChildren}) {
   return (
     <RecoilRoot>
-      <PlausibleProvider domain="mae.wtf" customDomain='metrics.mae.wtf'>
+      <PlausibleProvider domain="https://mae.wtf" customDomain='https://metrics.mae.wtf'>
         {children}
       </PlausibleProvider>
     </RecoilRoot>
