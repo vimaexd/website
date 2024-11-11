400ec76d823baf33e22a5336e484ad837c4ba141
vimaexd <mae@mae.wtf>
2024-11-11 10:13:28 +0000

    fix blog header again

--- src/app/(main)/blog/[post]/BlogPost.tsx
+++ src/app/(main)/blog/[post]/BlogPost.tsx
@@ -4,10 +4,13 @@ import Image from 'next/image';
 import dayjs from 'dayjs';
 
 export default function BlogPost({content, meta}: {content: any; meta: any;}) {
+  let pageContent = `<h1>${meta.title}</h1>
+    ${content.toString()}
+    <hr/>
+  `
   return (
     <PageContainer>
       <div className="flex gap-8 flex-col lg:flex-row items-center lg:items-start">
-        <h1>{meta.title}</h1>
         <div
           dangerouslySetInnerHTML={{ __html: content.toString() + "<hr/>" }}
           className="prose prose-invert w-full flex-shrink-0"
