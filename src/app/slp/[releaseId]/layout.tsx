607fa3db14ecb3147a52902e46b76757739d71c7
mae taylor <mae@mae.wtf>
2025-09-10 17:14:53 +0100

    add logo to slp

--- src/app/slp/[releaseId]/layout.tsx
+++ src/app/slp/[releaseId]/layout.tsx
@@ -1,10 +1,16 @@
 import { ComponentChildren } from '@mae/misc/types'
 import React, { Fragment } from 'react'
+import VimaeLogoMono from '@mae/assets/icons/logo-vimae-mono';
 
 export default function layout({children}: {children: ComponentChildren}) {
   return (
-    <div className='flex justify-center items-center h-screen w-screen'>
-      {children}
-    </div>
+    <>
+      <div className='flex justify-center items-center h-screen w-screen'>
+        {children}
+      </div>
+      <div className='absolute left-1/2 -translate-x-1/2 bottom-8'>
+        <VimaeLogoMono width="128px" height="fit"/>
+      </div>
+    </>
   )
 }
