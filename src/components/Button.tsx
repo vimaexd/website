bb15a592ef36d33d8422965617bbf533c0ed192a
mae taylor <mae@mae.wtf>
2025-09-10 15:59:50 +0100

    feat: button overhaul

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
-'use client'
+// 'use client'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Howl, Howler} from 'howler';
 
 const soundClick = new Howl({
@@ -9,7 +9,22 @@ const soundClick = new Howl({
   volume: 0.2
 });
 
-export default function Button({text, href, className, icon, onClick}: {text: string; href: string; className?: string; icon?: string; onClick?: () => any;}) {
+enum ButtonStyle {
+  Unstyled = 0,
+  Primary,
+  Active
+}
+
+export default function Button({text, href, className, icon, onClick, style = ButtonStyle.Primary}: 
+  {
+    text: string; 
+    href: string; 
+    className?: string; 
+    icon?: string; 
+    onClick?: () => any; 
+    style?: ButtonStyle;
+  }
+) {
   const pathname = usePathname();
 
   const playSound = () => {
@@ -17,16 +32,36 @@ export default function Button({text, href, className, icon, onClick}: {text: st
     soundClick.play()
   }
 
-
   if(onClick) {
     let href = null;
   }
 
+  let [classes, setClasses] = useState("");
+
+  useEffect(() => {
+    let temp = ""
+    temp += "select-none transition-all duration-75 ease-out py-1 px-4 rounded-md active:scale-90"
+    temp += " " + className
+
+    if(style == ButtonStyle.Unstyled) return;
+
+    switch(style) {
+      case ButtonStyle.Active:
+        temp += " " + "bg-ctp-mauve text-ctp-crust font-semibold"
+        break;
+
+      case ButtonStyle.Primary:
+        temp += " " + " bg-ctp-surface0 hover:bg-ctp-surface1 text-white"
+        break;
+    }
+
+    setClasses(temp)
+
+  }, [className, style])
+
   return (
     <Link href={href} onClick={playSound} className='inline-flex'>
-      <button className={
-        `${(pathname == href) ? 'bg-ctp-mauve text-ctp-crust font-semibold' : 'bg-ctp-surface0 hover:bg-ctp-surface1 text-white'} select-none transition-all duration-75 ease-out py-1 px-4 rounded-md active:scale-90 ${className || ""}`
-        }>
+      <button className={classes}>
         {icon && <i className={icon}></i>}
         {text}
       </button>
