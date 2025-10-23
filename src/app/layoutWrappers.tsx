054c3cba432314122be6e82840cd0a0170013796
mae taylor <mae@mae.wtf>
2025-10-23 13:43:30 +0100

    refactor: remove recoil wrapper

--- src/app/layoutWrappers.tsx
+++ src/app/layoutWrappers.tsx
@@ -1,15 +1,12 @@
 'use client';
 import { ComponentChildren } from '@mae/misc/types';
 import React from 'react'
-import { RecoilRoot } from 'recoil'
 import PlausibleProvider from 'next-plausible'
 
 export default function LayoutWrappers({children}: {children: React.ReactNode}) {
   return (
-    <RecoilRoot>
-      <PlausibleProvider domain="mae.wtf" customDomain='https://metrics.mae.wtf'>
+    <PlausibleProvider domain="mae.wtf" customDomain='https://metrics.mae.wtf'>
         {children}
-      </PlausibleProvider>
-    </RecoilRoot>
+    </PlausibleProvider>
   )
 }
