320b1685faef76ca036705497613e5f72d5cb652
vimae <34780309+vimaexd@users.noreply.github.com>
2026-03-23 12:37:09 +0000

    chore: nl presave redirect

--- next.config.js
+++ next.config.js
@@ -1,77 +1,81 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  productionBrowserSourceMaps: true,
-  async redirects() {
-    return [
-      {
-        source: "/socials",
-        destination: "/contact",
-        permanent: true
-      },
-      {
-        source: "/watercolor-stems",
-        destination: "https://drive.google.com/drive/folders/198X_ZzJQv7VeguTzRgJ6RJy8z9fihQr8?usp=sharing",
-        permanent: true
-      }
-    ]
-  },
-  images: {
-    remotePatterns: [
-      {
-        hostname: "cdn.mae.wtf"
-      },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    productionBrowserSourceMaps: true,
+    async redirects() {
+        return [
+            {
+                source: "/socials",
+                destination: "/contact",
+                permanent: true,
+            },
+            {
+                source: "/watercolor-stems",
+                destination:
+                    "https://drive.google.com/drive/folders/198X_ZzJQv7VeguTzRgJ6RJy8z9fihQr8?usp=sharing",
+                permanent: true,
+            },
+            {
+                source: "/nightlight",
+                destination: "https://hypeddit.com/h3fekt",
+            },
+        ];
+    },
+    images: {
+        remotePatterns: [
+            {
+                hostname: "cdn.mae.wtf",
+            },
 
-      // discord profile pictures
-      {
-        hostname: "cdn.discordapp.com"
-      },
-      {
-        hostname: "media.discordapp.net"
-      },
+            // discord profile pictures
+            {
+                hostname: "cdn.discordapp.com",
+            },
+            {
+                hostname: "media.discordapp.net",
+            },
 
-      // bandcamp profile pictures
-      {
-        hostname: "f4.bcbits.com"
-      },
+            // bandcamp profile pictures
+            {
+                hostname: "f4.bcbits.com",
+            },
 
-      // twitch.tv profile pictures
-      {
-        hostname: "static-cdn.jtvnw.net"
-      },
+            // twitch.tv profile pictures
+            {
+                hostname: "static-cdn.jtvnw.net",
+            },
 
-      // throne profile pictures
-      {
-        hostname: "thronecdn.com"
-      },
+            // throne profile pictures
+            {
+                hostname: "thronecdn.com",
+            },
 
-      // imgur (for musicbee album arts)
-      {
-        hostname: "i.imgur.com"
-      },
+            // imgur (for musicbee album arts)
+            {
+                hostname: "i.imgur.com",
+            },
 
-      // spotify (album art)
-      {
-        hostname: "i.scdn.co"
-      },
+            // spotify (album art)
+            {
+                hostname: "i.scdn.co",
+            },
 
-      // apple music (album art)
-      {
-        hostname: "*.mzstatic.com"
-      },
-      {
-        hostname: "rise.cider.sh"
-      },
+            // apple music (album art)
+            {
+                hostname: "*.mzstatic.com",
+            },
+            {
+                hostname: "rise.cider.sh",
+            },
 
-      // jellyfin (album art)
-      {
-        hostname: "jellyfin.maecafe.link"
-      }
-    ]
-  },
-}
+            // jellyfin (album art)
+            {
+                hostname: "jellyfin.maecafe.link",
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig;
